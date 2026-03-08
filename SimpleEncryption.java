aafd332dafb69b66efec385999b91906061f4806
Sachin <sachinkumar934122@gmail.com>
2026-03-08 15:21:42 +0530

    Refactor SimpleEncryption class for clarity and functionality

--- SimpleEncryption.java
+++ SimpleEncryption.java
@@ -1,60 +1,4 @@
-// import java.util.Scanner;
-
-// public class Self_doing_Encryption {
-//     public static void main(String[] args) {
-
-//         Scanner sc = new Scanner(System.in);
-//         String text = sc.nextLine();
-
-//         // initializing array for storing each characters
-//         char[] charArray = new char[text.length()];
-//         for(int i=0;i<text.length();i++){
-//             // convert text into characters
-//             charArray[i] = text.charAt(i);
-
-            
-//         }
-//         // input secret key 
-//         int key = sc.nextInt();
-
-//         char character  ;  // characters shoold be in capital letters.
-//         int charValue;
-        
-//         switch (charArray[i]) {
-//             case 'A': charValue = 0; break;
-//             case 'B': charValue = 1; break;
-//             case 'C': charValue = 2; break;
-//             case 'D': charValue = 3; break;
-//             case 'E': charValue = 4; break;
-//             case 'F': charValue = 5; break;
-//             case 'G': charValue = 6; break;
-//             case 'H': charValue = 7; break;
-//             case 'I': charValue = 8; break;
-//             case 'J': charValue = 9; break;
-//             case 'K': charValue = 10; break;
-//             case 'L': charValue = 11; break;
-//             case 'M': charValue = 12; break;
-//             case 'N': charValue = 13; break;
-//             case 'O': charValue = 14; break;
-//             case 'P': charValue = 15; break;
-//             case 'Q': charValue = 16; break;
-//             case 'R': charValue = 17; break;
-//             case 'S': charValue = 18; break;
-//             case 'T': charValue = 19; break;
-//             case 'U': charValue = 20; break;
-//             case 'V': charValue = 21; break;
-//             case 'W': charValue = 22; break;
-//             case 'X': charValue = 23; break;
-//             case 'Y': charValue = 24; break;
-//             case 'Z': charValue = 25; break;
-
-//             default: charValue = 0;
-//             }
-    
-//     }
-// }
-
-
+// simple java code to encrypt 26 characters
 import java.util.Scanner;
 
 public class SimpleEncryption {
@@ -105,4 +49,5 @@ public class SimpleEncryption {
 
         System.out.println("Encrypted Text: " + result);
     }
+
 }
